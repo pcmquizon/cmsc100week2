7783d6f42f5af48f18404f04da0e384c69feb557
pcmquizon <camillemendoza17@gmail.com>
2015-08-26 12:23:20 +0800

    week 3 yay~

--- config/router.js
+++ config/router.js
@@ -1,17 +1,24 @@
-// /config/oruter.js
+// /config/router.js
 var student = require('./../controllers/student'); //controllers -> response, callbacks
 var teacher = require('./../controllers/teacher'); //controllers -> response, callbacks
 
 /*
  export functions on other modules --> module.exports
  without, local use only
+ 
+ use only 1 module.exports and return
 */
 module.exports = function(router){
- router.route('/students') //app.route('/students')
-  .get(student.find);      //'get' http method, callback function
-                           //student.find is function(req,res){}
+ router.route('/students') //app.route('/students') , route handler
+  .get(student.find)      //'get' http method, callback function
+  .post(student.insert);  //student.find is function(req,res){}
  
- //when chaining routes do not add ';'
+ router.route('/students/:id')
+  .get(student.findOne)
+  .put(student.update)
+  .delete(student.remove);
+ 
+ //when chaining routes do not add ';' only at last route 
  router.route('/teachers')
   .get(teacher.find)
   .post(teacher.add);
--- controllers/student.js
+++ controllers/student.js
@@ -1,5 +1,51 @@
 // /controllers/student.js
 
-exports.find = function(req,res){ //lambda function, callback chuchu a while ago
- res.send('Hello World'); //in localhost:5000/students
+//import mysql
+var db = require(__dirname+'/../lib/mysql');
+
+//function(req,res) -> lambda function, callback chuchu a while ago
+// res.send('Hello World'); in localhost:5000/students
+
+//retrieve all
+exports.find = function(req,res,next){ 
+	 db.query("SELECT * FROM student", function(err, rows){ //for error handling (err)
+	 	if(err) return next(err);	//next(err) --> skip all route handlers 
+	 	res.send(rows);
+	 });
 };
+
+//? will have 3 param
+//query itself, last 2 other stuff you'll need
+//[req.params.id] gets the id in  router.route('/students/:id')
+exports.findOne = function(req,res,next){ 
+	 db.query("SELECT * FROM student WHERE id=?", [req.params.id], function(err, rows){
+	 	if(err) return next(err);
+	 	if(rows.length===0) res.status(404).send('Student not found.');
+	 	else res.send(rows[0]);	//returns first instance
+	 });
+};
+
+//info client sent
+exports.insert = function(req,res,next){ 
+	 db.query("INSERT INTO student(studno,name) VALUES (?,?)", [req.body.studno, req.body.name], function(err, rows){
+	 	if(err) return next(err);
+	 	res.send(rows);
+	 });
+};
+
+//in adv rest client http://localhost:5000/students/id param 1 will be used in id=?
+// SET ? for colname=value pairing
+exports.update = function(req,res,next){ 
+	 db.query("UPDATE student SET ? WHERE id=?", [req.body, req.params.id], function(err, rows){
+	 	if(err) return next(err);
+	 	res.send(rows);
+	 });
+};
+
+exports.remove = function(req,res,next){ 
+	 db.query("DELETE FROM student WHERE id=?", [req.params.id], function(err, rows){
+	 	if(err) return next(err);
+		res.send(rows);
+	 });
+};
+
